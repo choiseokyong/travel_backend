bfd06e680aef1f49340707bae2903f8c1011e810
최석용 <34879068+choiseokyong@users.noreply.github.com>
2025-08-01 15:00:29 +0900

    로그인 추가

--- travel/src/main/java/com/travel/controller/UserController.java
+++ travel/src/main/java/com/travel/controller/UserController.java
@@ -2,6 +2,10 @@ package com.travel.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.travel.domain.Plan;
+import com.travel.domain.LoginRequest;
+import com.travel.domain.LoginResponse;
 import com.travel.domain.User;
 import com.travel.service.UserService;
 
@@ -17,15 +22,31 @@ import com.travel.service.UserService;
 @RequestMapping("/users")
 public class UserController {
 	private final UserService userservice;
+	private final AuthenticationManager authenticationManager;
+//	private final JwtTokenProvider jwtTokenProvider;
 	
-	public UserController(UserService userservice) {
+	public UserController(UserService userservice,AuthenticationManager authenticationManager) {
 		this.userservice = userservice;
+		this.authenticationManager = authenticationManager;
 	}
 	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+		Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+//        String token = jwtTokenProvider.generateToken(authentication);
+//        return ResponseEntity.ok(new LoginResponse(token));
+		return null;
+	}
+	
+	// 회원 전체 조회
 	public List<User> getUserByNo() {
 		return userservice.getUserByNo();
 	}
 	
+	// 개인회원 조회
 	@GetMapping("/{userNo}")
 	public List<User> getUserByUserNo(@PathVariable int userNo) {
 		return userservice.getUserByUserNo(userNo);
