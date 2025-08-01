1eba7cd49c4f83c3d7ebce695a2b3353757d054d
최석용 <34879068+choiseokyong@users.noreply.github.com>
2025-08-01 14:59:08 +0900

    로그인 DTO 추가

---
+++ travel/src/main/java/com/travel/domain/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.travel.domain;
+
+public class LoginResponse {
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
